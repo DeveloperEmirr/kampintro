3fba311a7085d681cacb05b9f90b36e73e502099
DeveloperEmir <emir200134yilmaz@gmail.com>
2023-04-25 15:48:24 +0300

    25 Ocak Ders Sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriSil(Musteri musteri)
+        {
+            musteri.MusteriAdi = "";
+            musteri.MusteriSoyadi = "";
+            musteri.MusteriTc = "";
+        }
+
+        public void MusteriEkle(Musteri musterii)
+        {
+            /*
+             Bu kısımda müşteri bilgisi girdim elle diğer yerden ve müsteri ekleyince
+            alt kısımda ekranda göster dedim.
+             */
+            Console.WriteLine(musterii.MusteriTc + " \t" + musterii.MusteriAdi + " \t\t" + musterii.MusteriSoyadi);
+        }
+
+        public void MusteriListele(Musteri musteri, Musteri musteri2, Musteri musteri3)
+        {
+            /*
+             burada 3 tane müşteriyi çektim vaar olan bilerek böle çektim diğer tarafta
+            MusteriListele metodunu 3 kere çağırmıyayım diye 
+            ve burada her müsterilerin bilgilerini tablo şeklinde listeleme işlemini yaptırdım
+             */
+            Console.WriteLine("Tc\t\tAdı \t\tSoyadı");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine(musteri.MusteriTc+" \t"+ musteri.MusteriAdi+ " \t\t" + musteri.MusteriSoyadi);
+            Console.WriteLine(musteri2.MusteriTc + " \t" + musteri2.MusteriAdi + " \t\t" + musteri2.MusteriSoyadi);
+            Console.WriteLine(musteri3.MusteriTc + " \t" + musteri3.MusteriAdi + " \t\t" + musteri3.MusteriSoyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteribilgi = new Musteri();
+            musteribilgi.MusteriAdi = "Emir";
+            musteribilgi.MusteriSoyadi = "Yılmaz";
+            musteribilgi.MusteriTc = "12345678998";
+
+            Musteri musteribilgi2 = new Musteri();
+            musteribilgi2.MusteriAdi = "Ahmet";
+            musteribilgi2.MusteriSoyadi = "Öz";
+            musteribilgi2.MusteriTc = "9874563212";
+
+            Musteri musteribilgi3 = new Musteri();
+            musteribilgi3.MusteriAdi = "Efe";
+            musteribilgi3.MusteriSoyadi = "Sönmez";
+            musteribilgi3.MusteriTc = "36985214789";
+
+            Musteri musteribilgi4 = new Musteri();
+            musteribilgi4.MusteriAdi = "Ayşe";
+            musteribilgi4.MusteriSoyadi = "Gül";
+            musteribilgi4.MusteriTc = "741582369878";
+
+            MusteriManager manager = new MusteriManager();
+            manager.MusteriSil(musteribilgi);
+            manager.MusteriListele(musteribilgi, musteribilgi2, musteribilgi3);
+            manager.MusteriEkle(musteribilgi4);
+          
+            Console.ReadLine();
+        }
+    }
+}
